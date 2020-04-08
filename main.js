a35fcfdcef9ef3d35baf795504366d3ef538d8db
AlehZahorski <git3371m@gmail.com>
2020-04-08 19:43:47 +0200

    finish subtusk 1

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 let sizeSquare = document.getElementById('square');
-let px = 150;
+let px = 100;
+let square = true;
 function changeSize(){
-   px++;
    console.log("dziala funkcja ale jestem ciekaw w jaki sposob zrobic to powiekszenie kwadratu\nps:lol w 3 min ogarnelem to");
    sizeSquare.style.width = `${px}px`;
    sizeSquare.style.height = `${px}px`;
+   if(square == true){
+       px++;
+   }else{
+       px--;
+   }
    if(px == 300){
-       px = 150;
+       square=false;
+   }else if(px == 100){
+       square=true;
    }
 }
+  
+
 
 window.addEventListener('scroll', changeSize);
